1ba78d2a511a4b2a7b60d779a9fd39297cbefb49
YuriMelloFile <yurimello29@gmail.com>
2023-12-02 18:15:42 -0300

    banco funcional

--- conexao.php
+++ conexao.php
@@ -3,12 +3,21 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$db = "db_ph";
+$db = "db_php";
 
 $conexao = new mysqli($host, $user, $password, $db);
 
 
 if ($conexao->connect_error) {
-  echo 'nao foi possivel ' . $conexao->connect_error;
+  echo 'Não foi possivel conectar : ' . $conexao->connect_error;
+  exit();
 }
+
+$query = "SELECT * FROM usuarios WHERE nome = 'Bruno' LIMIT 1";
+
+$conexao->query($query);
+
+echo $conexao->affected_rows;
+
+
 ?>
